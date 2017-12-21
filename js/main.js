9c93bc1c998ae0193f2c830ae6325080667efedd
tylteng <teng.ty4@gmail.com>
2017-12-21 17:00:19 -0500

    light switch turns on and off

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var button = document.querySelector('.switch');
+  var message = document.querySelector('.status');
 
+  button.addEventListener('click', function() {
+    if (button.classList.contains('on')) {
+      button.classList.remove('on')
+      button.classList.add('off')
+      document.body.classList.remove('light')
+      document.body.classList.add('dark')
+      message.textContent = "Who turned the lights off?"
+    } else {
+      button.classList.remove('off')
+      button.classList.add('on')
+      document.body.classList.remove('dark')
+      document.body.classList.add('light')
+      message.textContent = "It's so bright in here!"
+    }
+  });
 
+});
